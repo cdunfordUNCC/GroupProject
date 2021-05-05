5135263679afef3d21128552b4f00b84d25b5d26
cdunfordUNCC <71344230+cdunfordUNCC@users.noreply.github.com>
2021-05-04 20:18:20 -0700

    Update Query1.sql

--- Query1.sql
+++ Query1.sql
@@ -2,7 +2,7 @@ use campus_eats_fall2020;
 select orders.restaurant_id, orders.total_price, orders.order_id, person.person_name,
 case when orders.total_price > 15.00 then 'The price is over $15.00.'
 else 'The price is under $15.00.'
-end as money
+end as price
 from orders
 join person
 group by order_id
